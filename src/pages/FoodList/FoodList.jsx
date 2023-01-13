466d72b60cc67d8a9010e2f36496f7c79f1b72c2
safirapm <safira.paramita.m@gmail.com>
2023-01-13 14:09:53 +0700

    Adding like features + user role views

--- src/pages/FoodList/FoodList.jsx
+++ src/pages/FoodList/FoodList.jsx
@@ -31,6 +31,48 @@ function FoodList() {
     getFood();
   }, []);
 
+  const handleLikes = (id, isLike) => {
+    if (!isLike) {
+      axios({
+        method: "post",
+        url: `${process.env.REACT_APP_BASEURL}api/v1/like`,
+        data: {
+          foodId: id,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          apiKey: process.env.REACT_APP_APIKEY,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          getFood();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios({
+        method: "post",
+        url: `${process.env.REACT_APP_BASEURL}api/v1/unlike`,
+        data: {
+          foodId: id,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          apiKey: process.env.REACT_APP_APIKEY,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+          getFood();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   const onSubmit = (values) => {
     axios({
       method: "post",
@@ -89,7 +131,7 @@ function FoodList() {
         <label htmlFor={props.id || props.name}>{label}</label>
         <input className="text-input form-control" {...field} {...props} />
         {meta.touched && meta.error ? (
-          <div className="text-error">{meta.error}</div>
+          <div className="no-input">{meta.error}</div>
         ) : null}
       </div>
     );
@@ -124,167 +166,176 @@ function FoodList() {
                         <i
                           className="bi bi-heart-fill"
                           style={{
-                            color: "#DD4A48",
+                            color: `${food.isLike ? "#DD4A48" : "#C8C6C6"}`,
+                            cursor: "pointer",
                           }}
+                          onClick={() => handleLikes(food.id, food.isLike)}
                         ></i>{" "}
                         {food.totalLikes}
                       </span>
                     </div>
-                    <div className="card-footer foodlist-button">
-                      <div className="btn-group" role="group">
-                        <Button
-                          className="delete-button"
-                          onClick={() => handleDelete(food.id)}
-                        >
-                          Delete
-                        </Button>
-                        <Button
-                          className="update-button"
-                          data-bs-toggle="modal"
-                          data-bs-target={`#staticBackdrop_${food.id}`}
-                        >
-                          Update
-                        </Button>
+
+                    {localStorage.getItem("role") === "admin" ? (
+                      <div className="card-footer foodlist-button">
+                        <div className="btn-group" role="group">
+                          <Button
+                            className="delete-button"
+                            onClick={() => handleDelete(food.id)}
+                          >
+                            Delete
+                          </Button>
+                          <Button
+                            className="update-button"
+                            data-bs-toggle="modal"
+                            data-bs-target={`#staticBackdrop_${food.id}`}
+                          >
+                            Update
+                          </Button>
+                        </div>
                       </div>
-                    </div>
+                    ) : null}
                   </div>
 
-                  <div
-                    className="modal fade"
-                    id={`staticBackdrop_${food.id}`}
-                    tabIndex="-1"
-                    aria-labelledby="modal-title"
-                    aria-hidden="true"
-                  >
-                    <div className="modal-dialog modal-md">
-                      <div className="modal-content">
-                        <div className="modal-body">
-                          <Formik
-                            initialValues={{
-                              name: food.name,
-                              description: food.description,
-                              imageUrl: savePicture,
-                              ingredients: food.ingredients,
-                              id: food.id,
-                            }}
-                            validationSchema={Yup.object({
-                              name: Yup.string().required("Required"),
-                              description: Yup.string().required("Required"),
-                            })}
-                            onSubmit={onSubmit}
-                          >
-                            <Container className="alluser-modal">
-                              <div
-                                className="text-center"
-                                style={{
-                                  fontFamily: "'General Sans', sans-serif",
-                                  paddingBottom: "5px",
-                                }}
-                              >
-                                <h3>Update Food</h3>
-                              </div>
+                  {localStorage.getItem("role") === "admin" ? (
+                    <div
+                      className="modal fade"
+                      id={`staticBackdrop_${food.id}`}
+                      tabIndex="-1"
+                      aria-labelledby="modal-title"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog modal-md">
+                        <div className="modal-content">
+                          <div className="modal-body">
+                            <Formik
+                              initialValues={{
+                                name: food.name,
+                                description: food.description,
+                                imageUrl: savePicture,
+                                ingredients: food.ingredients,
+                                id: food.id,
+                              }}
+                              validationSchema={Yup.object({
+                                name: Yup.string().required("Required"),
+                                description: Yup.string().required("Required"),
+                              })}
+                              onSubmit={onSubmit}
+                            >
+                              <Container className="alluser-modal">
+                                <div
+                                  className="text-center"
+                                  style={{
+                                    fontFamily: "'General Sans', sans-serif",
+                                    paddingBottom: "5px",
+                                  }}
+                                >
+                                  <h3>Update Food</h3>
+                                </div>
 
-                              <div className="alluser-box">
-                                <img src={food.imageUrl} alt={food.name} />
-                                <div className="row justify-content-center">
-                                  <div className="col-md-12">
-                                    <Form>
-                                      <MyTextInput
-                                        label="Food Name"
-                                        name="name"
-                                        type="text"
-                                        placeholder="Food name"
-                                      />
+                                <div className="alluser-box">
+                                  <img src={food.imageUrl} alt={food.name} />
+                                  <div className="row justify-content-center">
+                                    <div className="col-md-12">
+                                      <Form>
+                                        <MyTextInput
+                                          label="Food Name"
+                                          name="name"
+                                          type="text"
+                                          placeholder="Food name"
+                                        />
 
-                                      <MyTextInput
-                                        label="Food Description"
-                                        name="description"
-                                        type="text"
-                                        placeholder="Food Description"
-                                      />
+                                        <MyTextInput
+                                          label="Food Description"
+                                          name="description"
+                                          type="text"
+                                          placeholder="Food Description"
+                                        />
 
-                                      <div className="form-label">
-                                        Upload Picture (JPG/PNG/JPEG)
-                                      </div>
-                                      <AttachImage
-                                        name="imageUrl"
-                                        onChange={(value) =>
-                                          setSavePicture(value)
-                                        }
-                                      />
-                                      <div className="mb-3">
-                                        <label>Food Ingredients</label>
-                                        <FieldArray name="ingredients">
-                                          {(fieldArrayProps) => {
-                                            const { push, remove, form } =
-                                              fieldArrayProps;
-                                            const { values } = form;
-                                            const { ingredients } = values;
-                                            return (
-                                              <div>
-                                                {ingredients.map(
-                                                  (ingredient, index) => (
-                                                    <div
-                                                      key={index}
-                                                      className="d-flex input-group mb-1"
-                                                    >
-                                                      <Field
-                                                        name={`ingredients[${index}]`}
-                                                        placeholder="Food Ingredients"
-                                                        className="form-control"
-                                                      />
-                                                      {index > 0 && (
+                                        <div className="form-label">
+                                          Upload Picture (JPG/PNG/JPEG)
+                                        </div>
+                                        <AttachImage
+                                          name="imageUrl"
+                                          onChange={(value) =>
+                                            setSavePicture(value)
+                                          }
+                                        />
+                                        <div className="mb-3">
+                                          <label>Food Ingredients</label>
+                                          <FieldArray name="ingredients">
+                                            {(fieldArrayProps) => {
+                                              const { push, remove, form } =
+                                                fieldArrayProps;
+                                              const { values } = form;
+                                              const { ingredients } = values;
+                                              return (
+                                                <div>
+                                                  {ingredients.map(
+                                                    (ingredient, index) => (
+                                                      <div
+                                                        key={index}
+                                                        className="d-flex input-group mb-1"
+                                                      >
+                                                        <Field
+                                                          name={`ingredients[${index}]`}
+                                                          placeholder="Food Ingredients"
+                                                          className="form-control"
+                                                        />
+                                                        {index > 0 && (
+                                                          <button
+                                                            className="btn minus-button"
+                                                            type="button"
+                                                            onClick={() =>
+                                                              remove(index)
+                                                            }
+                                                          >
+                                                            -
+                                                          </button>
+                                                        )}
                                                         <button
-                                                          className="btn minus-button"
                                                           type="button"
+                                                          className="btn plus-button"
                                                           onClick={() =>
-                                                            remove(index)
+                                                            push("")
                                                           }
                                                         >
-                                                          -
+                                                          +
                                                         </button>
-                                                      )}
-                                                      <button
-                                                        type="button"
-                                                        className="btn plus-button"
-                                                        onClick={() => push("")}
-                                                      >
-                                                        +
-                                                      </button>
-                                                    </div>
-                                                  )
-                                                )}
-                                              </div>
-                                            );
-                                          }}
-                                        </FieldArray>
-                                      </div>
+                                                      </div>
+                                                    )
+                                                  )}
+                                                </div>
+                                              );
+                                            }}
+                                          </FieldArray>
+                                        </div>
 
-                                      <div className="text-center">
-                                        <Button
-                                          className="save-button"
-                                          type="submit"
-                                        >
-                                          Save Changes
-                                        </Button>
-                                        <Button
-                                          className="cancel-button"
-                                          data-bs-dismiss="modal"
-                                          aria-label="Close"
-                                        >
-                                          Cancel
-                                        </Button>
-                                      </div>
-                                    </Form>
+                                        <div className="text-center">
+                                          <Button
+                                            className="save-button"
+                                            type="submit"
+                                          >
+                                            Save Changes
+                                          </Button>
+                                          <Button
+                                            className="cancel-button"
+                                            data-bs-dismiss="modal"
+                                            aria-label="Close"
+                                          >
+                                            Cancel
+                                          </Button>
+                                        </div>
+                                      </Form>
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                            </Container>
-                          </Formik>
+                              </Container>
+                            </Formik>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  ) : null}
                 </Container>
               );
             })}
